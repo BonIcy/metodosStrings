42df6c666b61a2327246e016fa3f3dbde93acc63
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-03-07 20:33:27 -0500

    slice () substr() substring() split()

--- main.js
+++ main.js
@@ -1,32 +1,51 @@
 /* 
 METODOS  DE STRINGS:
 
-repeat() replace() trim()
+slice () substr() substring() split()
 
-El método repeat() devuelve una cadena con un número de copias de una cadena.
-El método repeat() devuelve una nueva cadena.
-El método repeat() no cambia la cadena original.
+El método slice() extrae una parte de una cadena.
+El método slice() devuelve la parte extraída en una nueva cadena.
+El método slice() no cambia la cadena original.
+Los parámetros de inicio y fin especifican la parte de la cadena que se va a extraer.
+La primera posición es 0, la segunda es 1, ...
+Un número negativo selecciona desde el final de la cadena.
 
-El método replace() busca en una cadena un valor o una expresión regular.
-El método replace() devuelve una nueva cadena con los valores reemplazados.
-El método replace() no cambia la cadena original.
+El método substr() extrae una parte de una cadena.
+El método substr() comienza en una posición específica y devuelve un número específico de caracteres.
+El método substr() no cambia la cadena original.
+Para extraer caracteres del final de la cadena, utilice una posición inicial negativa.
 
-El método trim() elimina los espacios en blanco de ambos lados de una cadena.
-El método trim() no cambia la cadena original.
+El método substring() extrae caracteres, entre dos índices (posiciones), de una cadena y devuelve la subcadena.
+El método substring() extrae caracteres de principio a fin (exclusivo).
+El método substring() no cambia la cadena original.
+Si el inicio es mayor que el final, los argumentos se intercambian: (4, 1) = (1, 4).
+Los valores iniciales o finales inferiores a 0 se tratan como 0.
 
+El método split() divide una cadena en una matriz de subcadenas.
+El método split() devuelve la nueva matriz.
+El método split() no cambia la cadena original.
+Si (" ") se usa como separador, la cadena se divide entre palabras.
 
- */
-let txt = "Hello world!";
-let resul = txt.repeat(4); // devuelve Hello world!Hello world!Hello world!Hello world!
+*/
+let textt = "Hello world!";
+let resultt = textt.slice(0, 5); //muestra Hello!
+let resulttt = textt.slice(3);  //muestra lo World!
 
-let textt = "Visit Microsoft!";
-let result = textt.replace("Microsoft", "Campus"); // muestra Visit Campus!
+let text = "Hello world!";
+let result = text.substr(1, 4); //deberia mostrar ello
+let resultw = text.substr(2); //deberia mostrar llo World!
 
-let text = "Mr Blue has a blue house and a blue car";
-let resulta = text.replace(/blue/g, "red"); //muestra Mr Blue has a red house and a red car
+let texta = "Hello world!";
+let resulta = texta.substring(1, 4); //mustra ell
+let resultaa = texta.substring(2); // muestra llo world!
 
-let texto = "       Hello World!        ";
-let results = texto.trim(); // muestra el Hello World! con espacio y luego uno sin expacios
-//otra forma de obtener el mismo resultado reemplazando los espacios con comillas vacias:
-let texti = "       Hello World!        ";
-let resulti = texti.replace(/^\s+|\s+$/gm,''); // muestra el Hello World! con espacio y luego uno sin expacios
+let texti = "How are you doing today?";
+const myArray = texti.split(" "); //divide una cadena en una array de subcadenas y devuelve la array: How,are,you,doing,today?
+
+let texte = "How are you doing today?";
+const myArrayy = texte.split(" ");
+let word = myArrayy[1]; //la palabra en la posicion 1 es "are", asi que eso muestra
+
+let textu = "How are you";
+const myArraye = textu.split(""); // reemplaza espacios  How,are,you
+const myArrayee = textu.split(" ", 2); //usa el parametro de limite : How,are
